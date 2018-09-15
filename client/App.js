f3ef0c4c1122e0b1d0405c080fab3aaa0ea3a87e
Lucas Harvey <lucasharvey@v1020-wn-10-134.campus-dynamic.uwaterloo.ca>
2018-09-15 03:17:33 -0400

    add chart

--- client/App.js
+++ client/App.js
@@ -1,37 +1,51 @@
 import React from 'react';
-import { LineChart, YAxis, Grid } from 'react-native-svg-charts'
+import { Grid, LineChart, XAxis, YAxis } from 'react-native-svg-charts'
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
 
-    const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
+        const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
 
-    const contentInset = { top: 20, bottom: 20 }
+        const axesSvg = { fontSize: 10, fill: 'grey' };
+        const verticalContentInset = { top: 10, bottom: 10 }
+        const xAxisHeight = 30
 
-    return (
-      <View style={{ height: 200, flexDirection: 'row' }}>
+        // Layout of an x-axis together with a y-axis is a problem that stems from flexbox.
+        // All react-native-svg-charts components support full flexbox and therefore all
+        // layout problems should be approached with the mindset "how would I layout regular Views with flex in this way".
+        // In order for us to align the axes correctly we must know the height of the x-axis or the width of the x-axis
+        // and then displace the other axis with just as many pixels. Simple but manual.
+
+        return (
+            <View style={{ height: 200, padding: 20, flexDirection: 'row' }}>
                 <YAxis
-                    data={ data }
-                    contentInset={ contentInset }
-                    svg={{
-                        fill: 'grey',
-                        fontSize: 10,
-                    }}
-                    numberOfTicks={ 10 }
-                    formatLabel={ value => `${value}ºC` }
+                    data={data}
+                    style={{ marginBottom: xAxisHeight }}
+                    contentInset={verticalContentInset}
+                    svg={axesSvg}
                 />
-                <LineChart
-                    style={{ flex: 1, marginLeft: 16 }}
-                    data={ data }
-                    svg={{ stroke: 'rgb(134, 65, 244)' }}
-                    contentInset={ contentInset }
-                >
-                    <Grid/>
-                </LineChart>
+                <View style={{ flex: 1, marginLeft: 10 }}>
+                    <LineChart
+                        style={{ flex: 1 }}
+                        data={data}
+                        contentInset={verticalContentInset}
+                        svg={{ stroke: 'rgb(134, 65, 244)' }}
+                    >
+                        <Grid/>
+                    </LineChart>
+                    <XAxis
+                        style={{ marginHorizontal: -10, height: xAxisHeight }}
+                        data={data}
+                        formatLabel={(value, index) => index}
+                        contentInset={{ left: 10, right: 10 }}
+                        svg={axesSvg}
+                    />
+                </View>
             </View>
-    );
-  }
+        )
+    }
+
 }
 
 const styles = StyleSheet.create({
